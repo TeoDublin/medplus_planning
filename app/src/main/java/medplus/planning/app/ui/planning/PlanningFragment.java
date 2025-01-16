c79d3ad2ed651e9fbbde492808037bfa2dcb32c9
TeoDublin <teodublin@gmail.com>
2025-01-16 23:09:05 +0100

    inp

--- app/src/main/java/medplus/planning/app/ui/planning/PlanningFragment.java
+++ app/src/main/java/medplus/planning/app/ui/planning/PlanningFragment.java
@@ -1,4 +1,4 @@
-package medplus.planning.app.ui.home;
+package medplus.planning.app.ui.planning;
 
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -10,22 +10,22 @@ import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
-import medplus.planning.app.databinding.FragmentHomeBinding;
+import medplus.planning.app.databinding.FragmentPlanningBinding;
 
-public class HomeFragment extends Fragment {
+public class PlanningFragment extends Fragment {
 
-    private FragmentHomeBinding binding;
+    private FragmentPlanningBinding binding;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
-                             ViewGroup container, Bundle savedInstanceState) {
-        HomeViewModel homeViewModel =
-                new ViewModelProvider(this).get(HomeViewModel.class);
+                            ViewGroup container, Bundle savedInstanceState) {
+        PlanningViewModel PlanningViewModel =
+                new ViewModelProvider(this).get(PlanningViewModel.class);
 
-        binding = FragmentHomeBinding.inflate(inflater, container, false);
+        binding = FragmentPlanningBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
         final TextView textView = binding.textHome;
-        homeViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+        PlanningViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
         return root;
     }
 
